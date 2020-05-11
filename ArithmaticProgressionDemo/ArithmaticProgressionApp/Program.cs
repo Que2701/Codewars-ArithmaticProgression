0f0664bc54b433a58f7f1e300343ebdfb09ee39a
tumelo <tumelom@evolvemed.co.za>
2020-05-11 20:43:28 +0200

    added solution

---
+++ ArithmaticProgressionDemo/ArithmaticProgressionApp/Program.cs
@@ -0,0 +1,19 @@
+﻿using ProgressionLibrary;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ArithmaticProgressionApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Progression.FindMissingProgression(new List<int> { 1, 5, 7, 9, 11 }));
+            // Allow the user to close the application
+            Console.ReadLine();
+        }
+    }
+}
---
+++ ArithmaticProgressionDemo/ProgressionLibrary/Progression.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ProgressionLibrary
+{
+    public class Progression
+    {
+        public static int FindMissingProgression(List<int> numbers)
+        {
+            var step = (numbers.Last() - numbers.First()) / numbers.Count;
+            int current = numbers.First();
+
+            for (int i = 0; i < numbers.Count; i++)
+            {
+                if (numbers[i] != current)
+                {
+                    return current;
+                }
+                
+                current += step;
+            }
+
+            throw new ArgumentException("No missing values were found in the provided list.");
+        }
+    }
+}
